b7b58ee5194db01a61830b081c5a4d20dbc5ba35
C V CHARAN <cherryiiit1234@gmail.com>
2025-07-02 22:19:59 +0530

    fix : make remove image to work for respoctive roles

--- server/src/features/profile/controllers/profileImageController.js
+++ server/src/features/profile/controllers/profileImageController.js
@@ -47,9 +47,15 @@ export const removeProfileImage = async (req, res) => {
   try {
     const userId = req.user.id;
     const userType = req.user.role;
-    // Use the dedicated remove service to set the user's photoUrl to the default image URL
-    const updatedUser = await removeProfileImageService(userId, userType);
-    res.json({ url: DEFAULT_PROFILE_IMAGE_URL });
+    // Set the correct default image URL based on user type
+    const DEFAULTS = {
+      customer: 'https://i.pinimg.com/736x/cf/3f/5c/cf3f5c9f2d8362f9111cf7f7c93cf42f.jpg',
+      admin:    'https://i.pinimg.com/736x/cf/3f/5c/cf3f5c9f2d8362f9111cf7f7c93cf42f.jpg',
+      driver:   'https://th-i.thgim.com/public/entertainment/movies/opp827/article25525252.ece/alternates/FREE_1200/Taxiwala'
+    };
+    const defaultUrl = DEFAULTS[userType] || DEFAULTS.customer;
+    const updatedUser = await removeProfileImageService(userId, userType, defaultUrl);
+    res.json({ url: defaultUrl });
   } catch (err) {
     res.status(500).json({ error: PROFILE_IMAGE_REMOVE_ERROR });
   }
